41fd60b95fb7a6b206b01a14e2702e839286dd06
unknown <birk.solbjorg@gmail.com>
2017-11-07 12:39:02 +0100

    added is it scary sensob

--- basic_robot/sensobs/is_it_scary.py
+++ basic_robot/sensobs/is_it_scary.py
@@ -1,7 +1,23 @@
 import framework
+from PIL import Image
+from imager2 import Imager
+from camera import Camera
+import itertools
 
-class IsScary(framework.Behaviour):
-    def __init__(self, bbcon, priority):
+class IsScary(framework.Sensob):
+    def __init__(self, camera: Camera):
+        super().__init__()
+        self.sensors.append(camera)
 
+    def average_shade(self, image: Image):
+        imager = Imager(image=image)
+        colour_sum = 0
+        for i in range(imager.xmax):
+            for j in range(imager.ymax):
+                colour_sum += sum(imager.get_pixel(i,j))/3
+        return colour_sum/(imager.xmax*imager.ymax)
+
+    def set_value(self):
+        self.value = self.average_shade(self.sensors[0].get_value())/255
 
 scaredycat = IsScary()
