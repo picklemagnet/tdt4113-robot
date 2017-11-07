9295cc81ff1e24d537b7350b9b091987c537ccc8
al3xknutsen <al3xknutsen@gmail.com>
2017-11-07 16:45:06 +0100

    Read values from ultra-sensor

--- basic_robot/framework.py
+++ basic_robot/framework.py
@@ -1,5 +1,5 @@
 from random import randint
-class Arbitrator:
+class BBCON:
     def __init__(self):
         self.behaviours = []
         self.active_behaviours = []
@@ -22,18 +22,18 @@ class Arbitrator:
     def run_one_timestep(self):
         pass
 
-    def choose_action(self, actions,deterministic=True):
+    def choose_action(self, actions, deterministic=True):
         if deterministic:
             return max(actions)[1]
         else:
             s = sum(zip(*actions).__next__())
             weighted = [x[0] for x in actions]
-            rand_act = randint(0,100)
+            rand_act = randint(0, 100)
             for i in range(len(weighted)):
-                weighted[i] = 100*weighted[i]/s
-                if i>0:
+                weighted[i] = 100 * weighted[i] / s
+                if i > 0:
                     weighted[i] += weighted[i-1]
-                if weighted[i]>=rand_act:
+                if weighted[i] >= rand_act:
                     return actions[i][1]
         # Return tuple containing motor recommendations (one per motob) and a boolean indicating
         # whether or not the run should be halted
--- basic_robot/sensobs/is_it_close.py
+++ basic_robot/sensobs/is_it_close.py
@@ -1,4 +1,4 @@
-from framework import *
+from framework import Sensob
 from utilities.ultrasonic import Ultrasonic
 from utilities.reflectance_sensors import ReflectanceSensors
 
@@ -6,16 +6,21 @@ from utilities.reflectance_sensors import ReflectanceSensors
 class IsItClose(Sensob):
     def __init__(self):
         super().__init__()
-        self.sensors = [Ultrasonic(), ReflectanceSensors()]
+        self.sensors = [Ultrasonic, ReflectanceSensors]
+        self.ultra = self.sensors[0]()
+        self.reflectance = self.sensors[1]()
 
     def set_value(self):
-        ultra, reflectance = self.sensors
-        #ultra.update()
-        value_ultra = ultra.get_value()
+        self.ultra.update()
+        value_ultra = self.ultra.get_value()
 
         #reflectance.update()
-        value_reflectance = reflectance.get_value()
+        #value_reflectance = reflectance.get_value()
+
+        print(value_ultra)
 
 
 isitclose = IsItClose()
 isitclose.sensors = [Ultrasonic, ReflectanceSensors]
+while True:
+    isitclose.set_value()
