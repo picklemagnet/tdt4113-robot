a01330afd85025093fe57e24145294900a915e1c
al3xknutsen <al3xknutsen@gmail.com>
2017-11-16 18:11:53 +0100

    updated not scared to not use random_step

--- basic_robot/motobs/not_scared.py
+++ basic_robot/motobs/not_scared.py
@@ -1,10 +1,11 @@
 from framework import Motob, random_step
 from utilities.motors import Motors
+from random import randint
 
 class NotScared(Motob):
     def __init__(self, motors):
         self.motors = motors
-        self.value = 0
+        self.value = 0.3
 
     def update(self):
         self.operationalize()
@@ -12,4 +13,20 @@ class NotScared(Motob):
     #value = (steps[,speed[,duration]])
     def operationalize(self):
         print("I am not scared")
-        random_step(self.motors)
+        speed = self.value
+        i = randint(1, 3):
+        if(i == 1):
+            self.motors.left(speed=speed, dur = 0.25)
+            self.motors.forward(speed=speed, dur = 0.75)
+        elif(i == 2):
+            self.motors.right(speed=speed, dur=0.25)
+            self.motors.forward(speed=speed, dur=0.75)
+        elif(i == 3):
+            self.motors.right(speed=speed, dur=0.5)
+            self.motors.left(speed=speed, dur=0.5)
+        else:
+            self.motors.left(speed=speed, dur=0.25)
+            self.motors.right(speed=speed, dur=0.25)
+            self.motors.left(speed=speed, dur=0.25)
+            self.motors.right(speed=speed, dur=0.25)
+
