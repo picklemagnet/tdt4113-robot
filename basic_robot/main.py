fe19c04be747bfad0405aa6431d586a56e61dbc3
al3xknutsen <al3xknutsen@gmail.com>
2017-11-16 15:31:26 +0100

    index error...

--- basic_robot/main.py
+++ basic_robot/main.py
@@ -25,8 +25,8 @@ def main():
               FleeBehaviour(sensobs[0], sensobs[1]),
               StartMoving(sensobs[2]),
               Terrified(sensobs[0], sensobs[1])]
-    
-    behavs.append(StopMoving(behavs[4], sensobs[2]))
+
+    behavs.append(StopMoving(behavs[2], sensobs[2]))
     motobs = [HyperScared, NotScared, Scared, Stop]
 
     for sensob in sensobs:
