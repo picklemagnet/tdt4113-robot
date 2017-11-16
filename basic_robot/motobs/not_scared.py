d1e9b6105e88279f55c96cf4091cbcdaec884159
al3xknutsen <al3xknutsen@gmail.com>
2017-11-16 15:37:54 +0100

    added motors to not_scared

--- basic_robot/motobs/not_scared.py
+++ basic_robot/motobs/not_scared.py
@@ -1,12 +1,14 @@
 from framework import Motob, random_step
+from utilities.motors import Motors
 
 class NotScared(Motob):
     def __init__(self):
         self.value = 0
+        self.motors = Motors()
 
     def update(self):
         self.operationalize()
 
     #value = (steps[,speed[,duration]])
     def operationalize(self):
-        random_step()
+        random_step(self.motors)
